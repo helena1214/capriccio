d1f700916ab0f9117a030bfa1c0f866186bbaeb4
Helena WONG <98277741+helena1214@users.noreply.github.com>
2022-03-24 00:46:26 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '7939fc7760854ad481bd45d88c7db8a1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: "Helena's Capriccio",
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Helena',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Sims4 mod, translation, arts and more.',
+  socialImageTitle: "Helena's Capriccio",
+  socialImageSubtitle: "Helena's Capriccio",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'HelenaWsims3',
+  github: 'helena1214',
+  linkedin: 'wongtszkwanhelena',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
